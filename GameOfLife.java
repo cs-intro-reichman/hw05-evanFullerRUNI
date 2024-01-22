a9ab75f6ecc93a1eee009f93728fdfc5e7eeaa71
evanFullerRUNI <evan.fuller@post.runi.ac.il>
2024-01-22 23:00:41 +0200

    HW5 commit V2

--- GameOfLife.java
+++ GameOfLife.java
@@ -11,7 +11,7 @@ public class GameOfLife {
 		String fileName = args[0];
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
-		//test1(fileName);
+		test1(fileName);
 		//test2(fileName);
 		//test3(fileName, 3);
 		//play(fileName);
@@ -142,12 +142,12 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		int rows = arr.length - 1;
 		int cols = arr[0].length - 1;
-		
+
 		for (int i = 1; i < rows; i++) {
 			for (int j = 1; j < cols; j++) {
-				System.out.print(arr[i][j] + " ");
+				System.out.print(" " + arr[i][j]);
 			}
-			System.out.println();
+			System.out.println(" ");
 		}
 	}
 		
